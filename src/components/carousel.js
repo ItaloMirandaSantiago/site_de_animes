5fc880c75c2721d5f07a6b4afa7e99c21525b734
ItaloMirandaSantiago <italo.miranda8635@gmail.com>
2022-07-23 23:05:43 -0300

    carousel com barra de rolagem

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -6,21 +6,22 @@ export default function Carousel(props) {
 
 useEffect(()=>{
     console.log(MoverCarousel)
-    if (MoverCarousel >= -62 && MoverCarousel <= 2) {
-        document.querySelector(".container").style.transform =`translateX(${MoverCarousel}%)`   
+
+    if (MoverCarousel >= -62 && MoverCarousel <= 0) {
+       // document.querySelector(".container").style.transform =`translateX(${MoverCarousel}%)`   
     }else if (MoverCarousel <= -62) {
-        SetMoverCarousel(-62)
-        document.querySelector(".container").style.transform =`translateX(-56%)`   
-    }else if (MoverCarousel >= 2){
-        SetMoverCarousel(2)
+      //  SetMoverCarousel(-62)
+    //    document.querySelector(".container").style.transform =`translateX(-56%)`   
+    }else if (MoverCarousel >= 0){
+  //      SetMoverCarousel(0)
     }
 }, [MoverCarousel])
 
     return(
         <div className="carousel">
+            <button className="button_esquerda" onClick={()=>SetMoverCarousel((e)=>e+10)}><img alt="seta esquerda" width="50px" src="https://cdn-icons-png.flaticon.com/128/271/271220.png" /></button>
             <div className="container">
-            <button className="button_esquerda" onClick={()=>SetMoverCarousel((e)=>e+10)}><img width="50px" src="https://cdn-icons-png.flaticon.com/128/271/271220.png" /></button>
-                {props.animes.data.map((item)=>(
+                    {props.animes.data.map((item)=>(
                     <li key={item.id} className={item.id}>
                         <button className="img_comprimida" onClick={()=>props.verification(item)}>
                             <img src={item.attributes.posterImage.tiny} 
@@ -30,8 +31,8 @@ useEffect(()=>{
                         </button>
                     </li>
                     ))}
-            <button className="button_direita" onClick={()=>SetMoverCarousel((e)=>e-10)}><img width="50px" src="https://cdn-icons-png.flaticon.com/128/271/271228.png" /></button>
             </div>
+            <button className="button_direita" onClick={()=>SetMoverCarousel((e)=>e-10)}><img alt="seta direita" width="50px" src="https://cdn-icons-png.flaticon.com/128/271/271228.png" /></button>
         </div>
     )
 }
